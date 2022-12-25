58d322dff72b3b619dfed07c1b7218b22805612c
Hamdala Tamirat <98209561+hamdegithub@users.noreply.github.com>
2022-12-25 03:19:06 -0800

    tuy
    ut7u

--- src/Axios.js
+++ src/Axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const Axios = () => {
   return axios.create({
-    baseURL: "https://back-uqt1.onrender.com/", // the url of our server
+    baseURL: "https://back-uqt1.onrender.com", // the url of our server
   });
 };
 
